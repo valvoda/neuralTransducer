fa6e9a3b40fd1df6f5d0b815536f23dbf668f9a0
valvoda <pepa.valvoda@gmail.com>
2021-01-13 21:28:15 +0000

    .

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Job details
-TIME=24:00  # HH:MM (default: 04:00, max: 240:00)
+TIME=12:00  # HH:MM (default: 04:00, max: 240:00)
 NUM_GPUS=1  # GPUs per node
 GPU_MODEL=TeslaV100_SXM2_32GB #GeForceGTX1080Ti  # Choices: GeForceGTX1080,GeForceGTX1080Ti,GeForceRTX2080Ti,TeslaV100_SXM2_32GB
 NUM_CPUS=1  # Number of cores (default: 1)
@@ -23,29 +23,35 @@ for arch in soft hard softinputfeed largesoftinputfeed approxihard \
 approxihardinputfeed hmm hmmfull transformer universaltransformer \
 tagtransformer taguniversaltransformer
 do
-  # Submit job
-  bsub -W $TIME \
-       -n $NUM_CPUS \
-       -R "rusage[mem=${CPU_RAM},ngpus_excl_p=${NUM_GPUS}]" \
-       -R "select[gpu_model0==${GPU_MODEL}]" \
-       -R "select[gpu_mtotal0>=30000]" \
-       "source ~/.bashrc; \
-       conda activate precedent; \
-       python src/train.py --dataset scan --train ${train_dir} --dev ${train_dir} --test ${test_dir}  --model model/basic/${arch} --embed_dim 100 \
-       --src_hs 200 \
-       --trg_hs 200 \
-       --dropout 0.5 \
-       --src_layer 2 \
-       --trg_layer 2 \
-       --max_norm 5 \
-       --shuffle \
-       --arch ${arch} \
-       --gpuid 0 \
-       --estop 1e-8 \
-       --epochs 50 \
-       --bs 512 \
-       --cleanup_anyway \
-       --total_eval 1 \
-       --max_steps 100000"
+  for train_dir in exp1 exp2
+  do
+    for experiment in {0..99}
+    do
+      # Submit job
+      bsub -W $TIME \
+           -n $NUM_CPUS \
+           -R "rusage[mem=${CPU_RAM},ngpus_excl_p=${NUM_GPUS}]" \
+           -R "select[gpu_model0==${GPU_MODEL}]" \
+           -R "select[gpu_mtotal0>=30000]" \
+           "source ~/.bashrc; \
+           conda activate precedent; \
+           python src/train.py --dataset scan --train ${train_dir}/${experiment}/tasks_train_simple.txt --dev ${train_dir}/${experiment}/tasks_train_simple.txt --test ${train_dir}/${experiment}/tasks_test_simple.txt  --model model/${train_dir}/${arch}/${experiment} --embed_dim 100 \
+           --src_hs 200 \
+           --trg_hs 200 \
+           --dropout 0.5 \
+           --src_layer 2 \
+           --trg_layer 2 \
+           --max_norm 5 \
+           --shuffle \
+           --arch ${arch} \
+           --gpuid 0 \
+           --estop 1e-8 \
+           --epochs 50 \
+           --bs 512 \
+           --cleanup_anyway \
+           --total_eval 1 \
+           --max_steps 100000"
+    done
+  done
 done
 
