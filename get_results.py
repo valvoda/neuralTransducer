846a0c70463cc5d85289e6f0ec86574f38953345
valvoda <pepa.valvoda@gmail.com>
2021-01-14 17:33:37 +0000

    .

--- get_results.py
+++ get_results.py
@@ -5,6 +5,8 @@ experiment/dataset/result_model1/result_model2...
 """
 
 import os
+import csv
+
 
 def run():
     paths = get_paths()
@@ -12,6 +14,27 @@ def run():
 
     for i, j in zip(paths,all_acc):
         print(i, j)
+
+def save_sheet(paths, all_acc):
+
+    for p in paths:
+        p = p[1:]
+        p = p.strip(".log")
+        model_type = p.split("/")[1:]
+        if len(model_type) == 2:
+            data_name = "scan"
+            model_name = model_type[1]
+            exp_type = "exp1"
+        else:
+            data_name = model_type[2]
+            model_name = model_type[1]
+            exp_type = model_type[0]
+
+    # with open('results.csv', mode='w') as r_file:
+    #     r_writer = csv.writer(r_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    #     r_writer.writerow([data_name, exp_type, soft, hard, softinputfeed, largesoftinputfeed, approxihard,
+    #                        approxihardinputfeed, hmm, hmmfull, transformer, universaltransformer, tagtransformer,
+    #                        taguniversaltransformer])
 
 def get_acc(paths):
     all_acc = []
