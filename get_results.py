6b6f0629996da7ae5f72e3e9f67e2005ee8a9644
valvoda <pepa.valvoda@gmail.com>
2021-01-14 13:50:31 +0000

    .

--- get_results.py
+++ get_results.py
@@ -13,17 +13,21 @@ def run():
         n_path = os.path.join(src_path, d)
         n_dirs = os.listdir(n_path)
         for nd in n_dirs:
-            nn_path = os.path.join(n_path, nd)
-            nn_dirs = os.listdir(nn_path)
-            for nnd in nn_dirs:
-                exp_path = os.path.join(nn_path, nnd)
+            try:
+                nn_path = os.path.join(n_path, nd)
+                nn_dirs = os.listdir(nn_path)
+                for nnd in nn_dirs:
+                    exp_path = os.path.join(nn_path, nnd)
+                    print(exp_path)
+                    # try:
+                    #     with open(exp_path, 'r') as f:
+                    #         lines = f.readlines()
+                    #         lines = [l.strip("\n") for l in lines]
+                    #         print(lines[-1])
+                    # except:
+                    #     pass
+            except:
+                exp_path = os.path.join(n_path, nd)
                 print(exp_path)
-                # try:
-                #     with open(exp_path, 'r') as f:
-                #         lines = f.readlines()
-                #         lines = [l.strip("\n") for l in lines]
-                #         print(lines[-1])
-                # except:
-                #     pass
 if __name__ == '__main__':
     run()
