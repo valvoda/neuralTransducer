528f0eada90d0d927aa5f55f1bc161d0968ff224
valvoda <pepa.valvoda@gmail.com>
2021-01-26 15:54:52 +0000

    .

--- run.sh
+++ run.sh
@@ -18,13 +18,13 @@ for arch in soft
 #approxihardinputfeed hmm hmmfull transformer universaltransformer \
 #tagtransformer taguniversaltransformer
 do
-  for experiment in 2 3
+  for experiment in {0..9}
   do
-    for size in 50 300 100
+    for size in 200
     do
       # 10exp1_run 20exp1_run 30exp1_run 40exp1_run 60exp1_run 70exp1_run 80exp1_run 90exp1_run 50exp1_run 100exp1_run 200exp1_run 400exp1_run 600exp1_run 800exp1_run 1000exp1_run 200exp1_run 300exp1_run 500exp1_run 700exp1_run 900exp1_run
       # 50exp2_run 100exp2_run 200exp2_run 400exp2_run 600exp2_run 800exp2_run 1000exp2_run 200exp2_run 300exp2_run 500exp2_run 700exp2_run 900exp2_run
-      for train_dir in 26exp1_run 30exp1_run 36exp1_run
+      for train_dir in 20exp1-loop
       do
         # Submit job
         bsub -W $TIME \
