fb4c4163a2386bdcbfcfde9ebc7974b2d84bfa6e
valvoda <pepa.valvoda@gmail.com>
2022-08-19 11:01:46 +0100

    .

--- test_gpu_run.sh
+++ test_gpu_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python src/train.py --dataset scan --train Dataset/29exp1_run/0/tasks_train_simple.txt --dev Dataset/29exp1_run/0/tasks_train_simple.txt --test Dataset/29exp1_run/0/tasks_train_simple.txt --src_hs 300 --trg_hs 300 --dropout 0.5 --src_layer 2 --trg_layer 2 --max_norm 5 --shuffle --arch soft --gpuid 0 --estop 1e-8 --epochs 50 --bs 512 --cleanup_anyway --total_eval 1 --max_steps 100000 --model model/300/29
+python src/train.py --dataset scan --train Dataset/29exp1_run/0/tasks_train_simple.txt --dev Dataset/29exp1_run/0/tasks_train_simple.txt --test Dataset/29exp1_run/0/tasks_train_simple.txt --src_hs 300 --trg_hs 300 --dropout 0.5 --src_layer 2 --trg_layer 2 --max_norm 5 --shuffle --arch soft --gpuid 0 --estop 1e-8 --epochs 5 --bs 512 --cleanup_anyway --total_eval 1 --max_steps 100 --model model/300/29
